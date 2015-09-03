049578d7d3023f827672ce11010846b56768a217
Dongjin Kim <tobetter@gmail.com>
2015-09-03 19:20:15 +0900

    Initial Android build commits for ODROID-C2
    Change-Id: I2219820ed464aa460c3a3a3d565178583bf45f34
    Signed-off-by: Dongjin Kim <tobetter@gmail.com>

---
+++ device.mk
@@ -0,0 +1,51 @@
+#
+# Copyright (C) 2013 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+PRODUCT_COPY_FILES += \
+    device/hardkernel/odroidc2/init.odroidc2.board.rc:root/init.odroidc2.board.rc \
+    device/hardkernel/odroidc2/init.odroidc2.usb.rc:root/init.odroidc2.usb.rc \
+    device/hardkernel/odroidc2/init.odroidc2.rc:root/init.odroidc2.rc \
+    device/hardkernel/odroidc2/ueventd.odroidc2.rc:root/ueventd.odroidc2.rc
+
+PRODUCT_COPY_FILES += \
+    device/hardkernel/odroidc2/files/media_profiles.xml:system/etc/media_profiles.xml \
+    device/hardkernel/odroidc2/files/audio_policy.conf:system/etc/audio_policy.conf \
+    device/hardkernel/odroidc2/files/media_codecs.xml:system/etc/media_codecs.xml \
+    device/hardkernel/odroidc2/files/mixer_paths.xml:system/etc/mixer_paths.xml \
+    device/hardkernel/odroidc2/files/mesondisplay.cfg:system/etc/mesondisplay.cfg
+
+# remote IME config file
+PRODUCT_COPY_FILES += \
+    device/hardkernel/odroidc2/files/remote.conf:system/etc/remote.conf \
+    device/hardkernel/odroidc2/files/Vendor_0001_Product_0001.kl:/system/usr/keylayout/Vendor_0001_Product_0001.kl
+
+PRODUCT_AAPT_CONFIG := xlarge hdpi xhdpi
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
+PRODUCT_CHARACTERISTICS := mbx,nosdcard
+
+DEVICE_PACKAGE_OVERLAYS := \
+    device/hardkernel/odroidc2/overlay
+
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+
+# setup dalvik vm configs.
+$(call inherit-product, frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-heap.mk)
+
+# set default USB configuration
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp
---
+++ odroidc2.mk
@@ -0,0 +1,131 @@
+# Copyright (C) 2015 Hardkernel Co., Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file is the build configuration for a full Android
+# build for Meson reference board.
+#
+
+# Dynamic enable start/stop zygote_secondary in 64bits
+# and 32bit system, default closed
+#TARGET_DYNAMIC_ZYGOTE_SECONDARY_ENABLE := true
+
+# Inherit from those products. Most specific first.
+ifneq ($(ANDROID_BUILD_TYPE), 32)
+ifeq ($(TARGET_DYNAMIC_ZYGOTE_SECONDARY_ENABLE), true)
+$(call inherit-product, device/hardkernel/common/dynamic_zygote_seondary/dynamic_zygote_64_bit.mk)
+else
+$(call inherit-product, build/target/product/core_64_bit.mk)
+endif
+endif
+
+$(call inherit-product, device/hardkernel/common/products/mbox/product_mbox.mk)
+$(call inherit-product, device/hardkernel/odroidc2/device.mk)
+
+# ODROID-C2:
+PRODUCT_MANUFACTURER := HardKernel Co., Ltd.
+PRODUCT_MODEL := ODROIDC2
+PRODUCT_BRAND := ODROID
+PRODUCT_DEVICE := odroidc2
+PRODUCT_NAME := odroidc2
+
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
+
+PRODUCT_PROPERTY_OVERRIDES += \
+        sys.fb.bits=32
+
+WITH_LIBPLAYER_MODULE := false
+
+PRODUCT_COPY_FILES += \
+    device/hardkernel/odroidc2/fstab.odroidc2:root/fstab.odroidc2
+
+#########################################################################
+#
+#                                                WiFi
+#
+#########################################################################
+
+WIFI_MODULE := bcm4354
+include device/hardkernel/common/wifi.mk
+
+# Change this to match target country
+# 11 North America; 14 Japan; 13 rest of world
+PRODUCT_DEFAULT_WIFI_CHANNELS := 11
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/wifi/config.txt:system/etc/wifi/4354/config.txt
+
+PRODUCT_PACKAGES += libbt-vendor
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml
+
+# Audio
+#
+BOARD_ALSA_AUDIO=tiny
+include device/hardkernel/common/audio.mk
+
+#########################################################################
+#
+#                                                Camera
+#
+#########################################################################
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml
+
+$(call inherit-product, device/hardkernel/common/media.mk)
+
+#########################################################################
+#
+#                                                Languages
+#
+#########################################################################
+
+# For all locales, $(call inherit-product, build/target/product/languages_full.mk)
+PRODUCT_LOCALES := en_AU en_US en_IN fr_FR it_IT es_ES et_EE de_DE nl_NL cs_CZ pl_PL ja_JP \
+  zh_TW zh_CN zh_HK ru_RU ko_KR nb_NO es_US da_DK el_GR tr_TR pt_PT pt_BR rm_CH sv_SE bg_BG \
+  ca_ES en_GB fi_FI hi_IN hr_HR hu_HU in_ID iw_IL lt_LT lv_LV ro_RO sk_SK sl_SI sr_RS uk_UA \
+  vi_VN tl_PH ar_EG fa_IR th_TH sw_TZ ms_MY af_ZA zu_ZA am_ET hi_IN en_XA ar_XB fr_CA km_KH \
+  lo_LA ne_NP si_LK mn_MN hy_AM az_AZ ka_GE my_MM mr_IN ml_IN is_IS mk_MK ky_KG eu_ES gl_ES \
+  bn_BD ta_IN kn_IN te_IN uz_UZ ur_PK kk_KZ
+
+#################################################################################
+#
+#                                                PPPOE
+#
+#################################################################################
+
+BUILD_WITH_PPPOE := true
+
+ifeq ($(BUILD_WITH_PPPOE),true)
+PRODUCT_PACKAGES += \
+    PPPoE \
+    libpppoejni \
+    libpppoe \
+    pppoe_wrapper \
+    pppoe \
+    droidlogic.frameworks.pppoe \
+    droidlogic.external.pppoe \
+    droidlogic.external.pppoe.xml
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.platform.has.pppoe=true
+endif
+
+#################################################################################
+#
+#                                                DEFAULT LOWMEMORYKILLER CONFIG
+#
+#################################################################################
+BUILD_WITH_LOWMEM_COMMON_CONFIG := true
+
+BOARD_USES_USB_PM := true
