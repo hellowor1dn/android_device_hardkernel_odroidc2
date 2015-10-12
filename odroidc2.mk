233d260d3a9d8c211b06fb5079f2e444face1b58
Dongjin Kim <tobetter@gmail.com>
2015-10-12 17:18:28 +0900

    ODROID-C2: change default Wifi channel to 11 from 13.
    Change-Id: Iec684502885df3d2ffa5556ea7043b91dbbfc08a
    Signed-off-by: Dongjin Kim <tobetter@gmail.com>

--- odroidc2.mk
+++ odroidc2.mk
@@ -75,7 +75,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Change this to match target country
 # 11 North America; 14 Japan; 13 rest of world
-PRODUCT_DEFAULT_WIFI_CHANNELS := 13
+PRODUCT_DEFAULT_WIFI_CHANNELS := 11
 
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
