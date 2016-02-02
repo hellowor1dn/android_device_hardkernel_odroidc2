7228163c98296072d80063b1f0d461b2f54059ce
codewalker <codewalker@hardkernel.com>
2016-02-02 14:21:08 +0900

    ODROID-C2: Fix to make odex for Development app.
    Change-Id: I8a403056d3e8d35ad13935f96fbe4b26df0b9206

---
+++ overlay/development/apps/Development/Android.mk
@@ -0,0 +1,16 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := eng
+
+LOCAL_JAVA_LIBRARIES := android.test.runner telephony-common
+
+LOCAL_SRC_FILES := $(call all-subdir-java-files) \
+                src/com/android/development/IRemoteService.aidl \
+
+LOCAL_PACKAGE_NAME := Development
+LOCAL_CERTIFICATE := platform
+
+LOCAL_DEX_PREOPT := true
+
+include $(BUILD_PACKAGE)
