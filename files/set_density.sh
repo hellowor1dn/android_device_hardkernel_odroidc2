5e70a9220b9e74afac168adeb2e7541c7986d819
codewalker <codewalker@hardkernel.com>
2016-04-08 09:38:51 +0900

    ODROID-C2: Support to change dynamically density.
    Change-Id: I57e29bf63e7a485dac2bb8cf02f67e155a3109fc

--- files/set_density.sh
+++ files/set_density.sh
@@ -3,6 +3,17 @@
 sleep 5
 SIZE="Error type 2"
 
+function get_orientation()
+{
+	ORIENTATION=`getprop | grep persist.demo.hdmirotation`
+	echo $ORIENTATION
+	if [ "$ORIENTATION" == "[persist.demo.hdmirotation]: [landscape]" ]; then
+		retval=1
+	else
+		retval=0
+	fi
+}
+
 while true
 do
 	case $SIZE in
@@ -13,15 +24,63 @@ do
 			if [ "$SIZE" == "" ]; then
 				SIZE="Error type 2"
 			fi
+			get_orientation
 		;;
 		Physical*)
-			if [ "$SIZE" == "Physical size: 800x480" ]; then
+			if [ "$SIZE" == "Physical size: 640x480" ]; then
 				wm density 120
 				break
-			else
+			elif [ "$SIZE" == "Physical size: 800x480" ]; then
+				if [ "$retval" == 1 ]; then
+					wm density 120
+				else
+					wm density 160
+				fi
+				break
+			elif [ "$SIZE" == "Physical size: 800x600" ]; then
 				wm density 160
 				break
+			elif [ "$SIZE" == "Physical size: 1024x600" ]; then
+				wm density 160
+				break
+			elif [ "$SIZE" == "Physical size: 1280x800" ]; then
+				if [ "$retval" == 1 ]; then
+					wm density 160
+				else
+					wm density 180
+				fi
+				break
+			elif [ "$SIZE" == "Physical size: 1280x1024" ]; then
+				if [ "$retval" == 1 ]; then
+					wm density 160
+				else
+					wm density 240
+				fi
+				break
+			elif [ "$SIZE" == "Physical size: 720x480" ]; then
+				wm density 120
+				break
+			elif [ "$SIZE" == "Physical size: 720x576" ]; then
+				if [ "$retval" == 1 ]; then
+					wm density 120
+				else
+					wm density 160
+				fi
+				break;
+			elif [ "$SIZE" == "Physical size: 1280x720" ]; then
+				if [ "$retval" == 1 ]; then
+					wm density 160
+				else
+					wm density 180
+				fi
+					break
+			elif [ "$SIZE" == "Physical size: 1920x1080" ]; then
+				if [ "$retval" == 1 ]; then
+					wm density 180
+				else
+					wm density 260
+				fi
+				break
 			fi
-		;;
 	esac
 done
---
+++ files/set_density2.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+SIZE=`wm size`
+
+ORIENTATION=`getprop | grep persist.demo.hdmirotation`
+echo $ORIENTATION
+if [ "$ORIENTATION" == "[persist.demo.hdmirotation]: [landscape]" ]; then
+	retval=1
+else
+	retval=0
+fi
+
+if [ "$SIZE" == "Physical size: 640x480" ]; then
+	wm density 120
+	break
+elif [ "$SIZE" == "Physical size: 800x480" ]; then
+	if [ "$retval" == 1 ]; then
+		wm density 120
+	else
+		wm density 160
+	fi
+	break
+elif [ "$SIZE" == "Physical size: 800x600" ]; then
+	wm density 160
+	break
+elif [ "$SIZE" == "Physical size: 1024x600" ]; then
+	wm density 160
+	break
+elif [ "$SIZE" == "Physical size: 1280x800" ]; then
+	if [ "$retval" == 1 ]; then
+		wm density 160
+	else
+		wm density 180
+	fi
+	break
+elif [ "$SIZE" == "Physical size: 1280x1024" ]; then
+	if [ "$retval" == 1 ]; then
+		wm density 160
+	else
+		wm density 240
+	fi
+	break
+elif [ "$SIZE" == "Physical size: 720x480" ]; then
+	wm density 120
+	break
+elif [ "$SIZE" == "Physical size: 720x576" ]; then
+	if [ "$retval" == 1 ]; then
+		wm density 120
+	else
+		wm density 160
+	fi
+	break;
+elif [ "$SIZE" == "Physical size: 1280x720" ]; then
+	if [ "$retval" == 1 ]; then
+		wm density 160
+	else
+		wm density 180
+	fi
+		break
+elif [ "$SIZE" == "Physical size: 1920x1080" ]; then
+	if [ "$retval" == 1 ]; then
+		wm density 180
+	else
+		wm density 260
+	fi
+	break
+fi
