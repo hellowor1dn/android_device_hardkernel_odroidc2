70d0405ddf4087c22d9dff216872a2385fdf6a43
codewalker <codewalker@hardkernel.com>
2016-04-19 17:49:24 +0900

    ODROID-C2: Ready to Support USP3.
    Change-Id: If7747d891a846956a066881c546ec2df8a5bf686

--- device.mk
+++ device.mk
@@ -34,6 +34,7 @@ PRODUCT_COPY_FILES += \
     device/hardkernel/odroidc2/files/set_density.sh:system/bin/set_density.sh \
     device/hardkernel/odroidc2/files/set_density2.sh:system/bin/set_density2.sh \
     device/hardkernel/odroidc2/files/rtc_sync.sh:system/bin/rtc_sync.sh \
+    device/hardkernel/odroidc2/files/ups3.sh:system/bin/ups3.sh \
     device/hardkernel/odroidc2/files/hardkernel-720.bmp.gz:system/etc/hardkernel-720.bmp.gz
 
 # remote IME config file
