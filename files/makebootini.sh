e9fbac9db4d24b60b0077be9cef0c732e399e0d3
Luke Go <luke.go@hardkernel.com>
2016-06-29 14:07:34 +0900

    ODROID-C2: Change the script execute timing.
    - Checking boot.ini Script be executed after bootcomplete.
    
    - It avoid to the potential unnecessary copy from boot.ini.template.
    
    Change-Id: Ic89726b99077fb8aab8559fbd6d350c3d5c94d38

--- files/makebootini.sh
+++ files/makebootini.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
-
-while true
-do
-    sleep 1
-    if [ -e "/storage/internal/boot.ini" ]
-    then
-        break
-    else
-        cp /system/etc/boot.ini.template /storage/internal/boot.ini
-    fi
-done
+if [ -e "/storage/internal/boot.ini" ]
+then
+    break
+else
+    cp /system/etc/boot.ini.template /storage/internal/boot.ini
+fi
