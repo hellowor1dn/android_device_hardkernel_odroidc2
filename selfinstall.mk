9871cc91e36116ede880bb20d2956f9730da201c
codewalker <codewalker@hardkernel.com>
2016-07-27 16:27:33 +0900

    ODROID-C2: Fixed build error for 64bit.
    Change-Id: I603ab4ea8ee16978293fabe54ff69a3d77032142

--- selfinstall.mk
+++ selfinstall.mk
@@ -26,8 +26,8 @@ SELFINSTALL_CACHE_IMAGE := $(SELFINSTALL_DIR)/cache.ext4
 # Bootloader
 #
 bootable/uboot/build/u-boot-orig.bin:
-	make -C bootable/uboot ARCH=$(TARGET_ARCH) $(TARGET_PRODUCT)_config
-	make -C bootable/uboot ARCH=$(TARGET_ARCH)
+	make -C bootable/uboot ARCH=arm $(TARGET_PRODUCT)_config
+	make -C bootable/uboot ARCH=arm
 
 .PHONY: $(PRODUCT_OUT)/bl1.bin.hardkernel
 $(PRODUCT_OUT)/bl1.bin.hardkernel:
