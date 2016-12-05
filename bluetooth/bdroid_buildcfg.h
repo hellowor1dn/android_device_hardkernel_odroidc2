004e299f1e10bb975d44175dffd4097059f7ccb4
Luke Go <luke.go@hardkernel.com>
2016-12-05 13:22:08 +0900

    ODROID-C2: Enabled the BLE.
    Change-Id: I9b16436fe055239bc224c25e478f42e5803ab4f0

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -20,19 +20,9 @@
 #define BTM_DEF_LOCAL_NAME "Odroid-c2"
 // At present either USB or UART is supporteed
 #define BLUETOOTH_HCI_USE_USB       TRUE
-// SERVICE_CLASS:0x5A (Bit17 -Networking,Bit19 - Capturing,Bit20 -Object Transfer,Bit22 -Telephony)
-// MAJOR CLASS: COMPUTER
-// MINOR CLASS: reserved (no minor class for mbox)
-#define BTA_DM_COD {0x5A, 0x01, 0x20}
-#define BLE_PRIVACY_SPT		FALSE
+
 // Bluetooth low Power Mode is supporteeed on BT4.0
 #define HCILP_INCLUDED      FALSE
-
-// 12.0 Updates
-#define BLE_INCLUDED FALSE
-#define BTA_GATT_INCLUDED FALSE
-#define SMP_INCLUDED FALSE
-#define PAN_NAP_DISABLED TRUE
 
 /* Enable A2DP sink */
 #define BTA_AV_SINK_INCLUDED TRUE
