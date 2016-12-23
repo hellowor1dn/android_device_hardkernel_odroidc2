ae98994c9882d780e408d3f45dce2eb8a4daedd5
codewalker <codewalker@hardkernel.com>
2016-12-23 17:33:59 +0900

    ODROID-C2: Add kernel builtin rtc in vfat partision.
    Change-Id: I204a642628a000726ccff6a53f37a3452f26427f

--- files/makebootini.sh
+++ files/makebootini.sh
@@ -5,3 +5,10 @@ then
 else
     cp /system/etc/boot.ini.template /storage/internal/boot.ini
 fi
+
+if [ -e "/storage/internal/Image" ]
+then
+    break
+else
+    cp /system/etc/Image /storage/internal/Image
+fi
