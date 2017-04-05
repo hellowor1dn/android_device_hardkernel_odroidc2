731836f7a5bb9751ed5317f07e42a0f6ade2ca7e
codewalker <codewalker@hardkernel.com>
2017-04-05 10:27:36 +0900

    ODROID-C2: Fix vu8 backlight control script.
    Change-Id: I3d582b5bb4be0c924365356d22ab34fb7f3944b2

--- files/vu8c_backlight_c2_android.sh
+++ files/vu8c_backlight_c2_android.sh
@@ -2,15 +2,11 @@
 
 path="/sys/class/gpio"
 
-echo 234 > $path/export
 echo 214 > $path/export
 
-echo out > $path/gpio234/direction
 echo out > $path/gpio214/direction
-echo 0 > $path/gpio234/value
 echo 0 > $path/gpio214/value
 
-chown system:system $path/gpio234/value
 chown system:system $path/gpio214/value
 
 cur_stat="On"
@@ -25,11 +21,9 @@ while [ 1 ]; do
         echo "monitor goes to Off"
         # backlight off first
         echo 1 > $path/gpio214/value
-        echo 1 > $path/gpio234/value
         cur_stat="Off"
     elif [[ $screen_info == *"ON"* && $cur_stat == "Off" ]]; then
         echo "monitor turns back On"
-        echo 0 > $path/gpio234/value
         # backlight on later
         echo 0 > $path/gpio214/value
         cur_stat="On"
